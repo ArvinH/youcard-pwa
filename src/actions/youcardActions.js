560d006ce2e4338ba3660136309ec9b770cacd6d
arvinh <arvin0731@gmail.com>
2016-09-27 20:59:08 +0800

    need to in same origin

--- src/actions/youcardActions.js
+++ src/actions/youcardActions.js
@@ -17,7 +17,7 @@ export const getCardStack = (cardstackId = '83775e20-83bb-11e6-b1c9-2727e3ec36ef
   return (dispatch) => {
     dispatch({ type: GET_YOUCARD_INITIATE });
     dispatch(showSpinner());
-    fetch(`http://localhost:1337/tw.yql.news.yahoo.com/v1/abunews/cardstack/${cardstackId}`, { mode: 'no-cors' })
+    fetch(`https://tw.yql.news.yahoo.com/v1/abunews/cardstack/${cardstackId}`)
       .then(response => response.json())
       .then((json) => {
         dispatch({ type: GET_YOUCARD_SUCCESS, payload: { data: json.cardstacks.result } });
@@ -31,7 +31,7 @@ export const getCardList = () => {
   return (dispatch) => {
     dispatch({ type: GET_YOUCARDLIST_INITIATE });
     dispatch(showSpinner());
-    fetch('http://localhost:1337/tw.yql.news.yahoo.com/v1/abunews/cardstack_search_advance')
+    fetch('https://tw.yql.news.yahoo.com/v1/abunews/cardstack_search_advance')
       .then(response => response.json())
       .then((json) => {
         dispatch({ type: GET_YOUCARDLIST_SUCCESS, payload: { data: json.cardstacks.result } });
